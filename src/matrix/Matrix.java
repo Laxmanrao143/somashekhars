f610a201d7350b9ff900e27b3087bd5cdbb04528
LAXMANRAO <laxmanrao9008@gmail.com>
2023-05-08 16:57:57 +0530

    asd

---
+++ src/matrix/Matrix.java
@@ -0,0 +1,82 @@
+package matrix;
+
+public class Matrix {
+
+	public static void main(String[] args) {
+		for(int i=1;i<=4;i++)
+		{
+			
+			for(int j=1;j<=4;j++)
+			{
+				
+				if(i==1&&j==1)
+				{
+					System.out.print( 1);
+				}
+				if(i==1&&j==2)
+				{
+					System.out.print(  5);
+				}
+				if(i==1&&j==3)
+				{
+					System.out.print( 9);
+				}
+				if(i==1&&j==4)
+				{
+					System.out.print( 13);
+				}
+				if(i==2&&j==1)
+				{
+					System.out.print( 14);
+				}
+				if(i==2&&j==2)
+				{
+					System.out.print( 10);
+				}
+				if(i==2&&j==3)
+				{
+					System.out.print( 6);
+				}
+				if(i==2&&j==4)
+				{
+					System.out.print( 2);
+				}
+				if(i==3&&j==1)
+				{
+					System.out.print( 3);
+				}
+				if(i==3&&j==2)
+				{
+					System.out.print( 7);
+				}
+				if(i==3&&j==3)
+				{
+					System.out.print( 11);
+				}
+				if(i==3&&j==4)
+				{
+					System.out.print( 15);
+				}
+				if(i==4&&j==1)
+				{
+					System.out.print( 16);
+				}
+				if(i==4&&j==2)
+				{
+					System.out.print( 12);
+				}
+				if(i==4&&j==3)
+				{
+					System.out.print (8);
+				}
+				if(i==4&&j==4)
+				{
+					System.out.print( 4   );
+				}
+			}
+			System.out.println();
+		}
+
+	}
+
+}
